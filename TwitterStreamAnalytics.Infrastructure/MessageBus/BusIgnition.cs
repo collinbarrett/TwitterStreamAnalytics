1b781f882bdd7e536b91e30c53cda3613d71cf88
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2022-08-15 02:00:12 -0500

    feat(infra): ✨📄 add logging / doc comments to BusIgnition

--- TwitterStreamAnalytics.Infrastructure/MessageBus/BusIgnition.cs
+++ TwitterStreamAnalytics.Infrastructure/MessageBus/BusIgnition.cs
@@ -1,24 +1,33 @@
 ﻿using MassTransit;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace TwitterStreamAnalytics.Infrastructure.MessageBus;
 
+/// <summary>
+///     The "ignition" worker of the MassTransit message bus.
+/// </summary>
+/// <seealso cref="IHostedService" />
 internal class BusIgnition : IHostedService
 {
     private readonly IBusControl _busControl;
+    private readonly ILogger<BusIgnition> _logger;
 
-    public BusIgnition(IBusControl busControl)
+    public BusIgnition(IBusControl busControl, ILogger<BusIgnition> logger)
     {
         _busControl = busControl;
+        _logger = logger;
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
+        _logger.LogDebug("Starting message bus.");
         return _busControl.StartAsync(cancellationToken);
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
+        _logger.LogDebug("Stopping message bus.");
         return _busControl.StopAsync(cancellationToken);
     }
 }
