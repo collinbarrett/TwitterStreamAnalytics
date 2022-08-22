02c3e73c23905ffc467d276d20a0f86983e971f2
Collin M. Barrett <collinbarrett@users.noreply.github.com>
2022-08-22 04:00:38 -0500

    test(api): 🧪 GET_Stats_AfterStartingStreamReaderProducingPredictableCountsWithConcurrentHashtagInstances_ReturnsPredictedCounts

--- tests/IntegrationTests/TwitterStreamAnalytics.Tests/TwitterStreamAnalyticsTests.cs
+++ tests/IntegrationTests/TwitterStreamAnalytics.Tests/TwitterStreamAnalyticsTests.cs
@@ -1,6 +1,13 @@
 using System.Net.Http.Json;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Tweetinvi;
+using Tweetinvi.Events.V2;
+using Tweetinvi.Models.V2;
+using Tweetinvi.Streaming.V2;
 
 namespace TwitterStreamAnalytics.Tests;
 
@@ -108,5 +115,67 @@ public class TwitterStreamAnalyticsTests : IClassFixture<WebApplicationFactory<P
         Assert.True(actual?.TweetCount > 0);
         Assert.True(actual.HashtagCount > 0);
         Assert.True(actual.TopHashtags.Count > 0);
+    }
+
+    [Fact]
+    public async Task
+        GET_Stats_AfterStartingStreamReaderProducingPredictableCountsWithConcurrentHashtagInstances_ReturnsPredictedCounts()
+    {
+        // Arrange
+        var mockTwitterClient = new Mock<ITwitterClient>();
+        var mockSampleStream = new Mock<ISampleStreamV2>();
+        mockTwitterClient.Setup(tc => tc.StreamsV2.CreateSampleStream()).Returns(mockSampleStream.Object);
+        var client = _factory.WithWebHostBuilder(
+                b => b.ConfigureTestServices(s => s.AddSingleton(_ => mockTwitterClient.Object))
+            )
+            .CreateClient();
+        var startResponse = await client.PostAsync(StartUrl, default);
+        startResponse.EnsureSuccessStatusCode();
+        const string testTag = "TestTag";
+        var mockTweet = new TweetV2ReceivedEventArgs(new TweetV2Response
+        {
+            Tweet = new TweetV2
+            {
+                Id = long.MaxValue.ToString(),
+                Entities = new TweetEntitiesV2
+                {
+                    Hashtags = new[]
+                    {
+                        new HashtagV2
+                        {
+                            Tag = testTag
+                        },
+                        new HashtagV2
+                        {
+                            Tag = testTag
+                        }
+                    }
+                }
+            }
+        }, default);
+        mockSampleStream.Raise(ss => ss.TweetReceived += null, EventArgs.Empty, mockTweet);
+        Thread.Sleep(1000);
+
+        // Act
+        var statsResponse = await client.GetAsync(StatsUrl);
+
+        // Assert
+        statsResponse.EnsureSuccessStatusCode();
+        var actual = await statsResponse.Content.ReadFromJsonAsync<Stats>();
+        var expected = new Stats
+        {
+            IsReadingStream = true,
+            TweetCount = 1,
+            HashtagCount = 1,
+            TopHashtags = new List<Hashtag>
+            {
+                new()
+                {
+                    Tag = testTag,
+                    Count = 2
+                }
+            }
+        };
+        actual.Should().BeEquivalentTo(expected);
     }
 }
